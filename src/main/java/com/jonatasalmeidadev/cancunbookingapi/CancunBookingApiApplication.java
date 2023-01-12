5c5bb57619ceb60e88a0e20bde657ac331b3c20a
Jonatas Almeida <jonatascalmeida@gmail.com>
2023-01-12 18:40:18 -0300

    Initial commit

---
+++ src/main/java/com/jonatasalmeidadev/cancunbookingapi/CancunBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.jonatasalmeidadev.cancunbookingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancunBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancunBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonatasalmeidadev/cancunbookingapi/CancunBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonatasalmeidadev.cancunbookingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancunBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
